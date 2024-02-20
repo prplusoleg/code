c3e1084e8bf8dd0055e9a740b82603cf52d3fa3b
prplusoleg <160644713+prplusoleg@users.noreply.github.com>
2024-02-20 21:49:51 +0200

    Create leeloo.js

---
+++ leeloo.js
@@ -0,0 +1,7 @@
+window.LEELOO = function(){
+    window.LEELOO_INIT = { id: '5fbb98a52b91b0000c83d834' };
+    var js = document.createElement('script');
+    js.src = 'https://app.leeloo.ai/init.js';
+    js.async = true;
+    document.getElementsByTagName('head')[0].appendChild(js);
+}; LEELOO();

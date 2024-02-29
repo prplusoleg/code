396f7dcc6bb6e51ce32092c7c307be968b06c622
prplusoleg <160644713+prplusoleg@users.noreply.github.com>
2024-02-29 19:36:15 +0200

    Update date.js

--- date.js
+++ date.js
@@ -1,1 +1,15 @@
-$(document).ready(function(){var t=new Date,e=new Date(t);e.setDate(t.getDate()+1);var n=new Date,a=n.getUTCHours()+(+3),r=new Array("січня","лютого","березня","квітня","травня","червня","липня","серпня","вересня","жовтня","листопада","грудня"),i=new Array("Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота");if(a>=18){$(".textwebinar").text(i[e.getDay()]+", "+e.getDate()+" "+r[e.getMonth()]+" у 19:00");}else{$(".textwebinar").text(i[t.getDay()]+", "+t.getDate()+" "+r[t.getMonth()]+" у 19:00");}});
+$(document).ready(function() {
+    var today = new Date();
+    var tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+
+    var currentHour = (new Date().getUTCHours() + 3); // Проверка часов для Украины
+    var months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+    var days = ["воскресенье", "понедельник", "вторник", "среду", "четверг", "пятницу", "субботу"];
+
+    if (currentHour >= 18) {
+        $(".textwebinar").text("завтра, " + tomorrow.getDate() + " " + months[tomorrow.getMonth()] + " у 19:00");
+    } else {
+        $(".textwebinar").text("сегодня, " + today.getDate() + " " + months[today.getMonth()] + " у 19:00");
+    }
+});

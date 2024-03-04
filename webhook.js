3fbf98a76fb9da69e4f74dce6baa1dc99c8a087d
prplusoleg <160644713+prplusoleg@users.noreply.github.com>
2024-03-04 20:35:24 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -7,37 +7,52 @@ Webflow.push(function() {
   $('form').submit(function(e) {
     e.preventDefault();
 
-  	const $form = $(this); // The submitted form
+    const $form = $(this); // The submitted form
     const $submit = $('[type=submit]', $form); // Submit button of form
     const buttonText = $submit.val(); // Original button text
     const buttonWaitingText = $submit.attr('data-wait'); // Waiting button text value
     const formMethod = $form.attr('method'); // Form method (where it submits to)
     const formAction = $form.attr('action'); // Form action (GET/POST)
     const formRedirect = $form.attr('data-redirect'); // Form redirect location
-    const formData = $form.serialize(); // Form data
-    
+    let formData = $form.serialize(); // Form data
+
+    // Extracting UTM parameters from URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const utmSource = urlParams.get('utm_source');
+    const utmMedium = urlParams.get('utm_medium');
+    const utmCampaign = urlParams.get('utm_campaign');
+    const utmContent = urlParams.get('utm_content');
+    const utmTerm = urlParams.get('utm_term');
+
+    // Adding UTM parameters to form data
+    formData += `&utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_content=${utmContent}&utm_term=${utmTerm}`;
+
+    // Adding form name to form data
+    const formName = $form.attr('name');
+    formData += `&formName=${formName}`;
+
     // Set waiting text
     if (buttonWaitingText) {
       $submit.val(buttonWaitingText); 
     }
     
     $.ajax(formAction, {
-    	data: formData,
+      data: formData,
       method: formMethod
     })
     .done((res) => {
       // If form redirect setting set, then use this and prevent any other actions
       if (formRedirect) { window.location = formRedirect; return; }
 
-    	$form
-      	.hide() // optional hiding of form
-    		.siblings('.w-form-done').show() // Show success
-      	.siblings('.w-form-fail').hide(); // Hide failure
+      $form
+        .hide() // optional hiding of form
+        .siblings('.w-form-done').show() // Show success
+        .siblings('.w-form-fail').hide(); // Hide failure
     })
     .fail((res) => {
       $form
-      	.siblings('.w-form-done').hide() // Hide success
-    	  .siblings('.w-form-fail').show(); // show failure
+        .siblings('.w-form-done').hide() // Hide success
+        .siblings('.w-form-fail').show(); // show failure
     })
     .always(() => {
       // Reset text
